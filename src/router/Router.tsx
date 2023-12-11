15276d2eb02c5a643cfe39025917de2506f23628
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-12 06:41:18 +0900

    404ページを設定

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Login } from '../components/pages/Login';
 import { homeRoutes } from './HomeRoutes';
+import { Page404 } from '../components/pages/Page404';
 
 export const Router: VFC = memo(() => {
   return (
@@ -26,6 +27,9 @@ export const Router: VFC = memo(() => {
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404/>
+      </Route>
     </Switch>
   );
 });
