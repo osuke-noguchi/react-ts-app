7ca6b0eb4ce8300393cfaa22c7b0cc8b826a8ea3
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-12 06:36:14 +0900

    ルートを設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-// 必要なモジュールをインポートします
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // スタイルシートのインポート
-import App from './App'; // App コンポーネントのインポート
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactコンポーネントをルートDOMノードにレンダリングします
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-// （オプション）Web Vitalsのレポートを行います
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
